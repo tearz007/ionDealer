804118ab7e088a3f13d4d27c3b0f978159bb5ef6
tearz007 <54117577+tearz007@users.noreply.github.com>
2020-11-24 03:30:40 -0800

    24/11/2020

--- src/app/pages/map/map.page.ts
+++ src/app/pages/map/map.page.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import MapboxGeocoder from '@mapbox/mapbox-gl-geocoder';
-import '@mapbox/mapbox-gl-geocoder/dist/mapbox-gl-geocoder.css';
+
 declare var mapboxgl;
+declare var  MapboxDirections;
 
 
 @Component({
@@ -10,25 +10,25 @@ declare var mapboxgl;
   styleUrls: ['./map.page.scss'],
 })
 export class MapPage implements OnInit {
-  map
-  constructor(
-
-  ) {}
+  map;
+  turf;
+  constructor() { }
 
   ngOnInit() {
     this.mapFunction();
     this.mapFunction2();
-    this.mapLocation();
-  //  this.map3D();
-   // this.getCurrentCoordinates();
+    // this.mapDistance();
+    // this.mapLocation();
+    // this.mapDirection();
   }
+
   mapFunction() {
     mapboxgl.accessToken = 'pk.eyJ1IjoidGVhcnoiLCJhIjoiY2toa2dqcmM3MWIwNjJ5cDlqazhyYzdteiJ9.jYlNVUpq4tkE1jva-mtyqg';
     this.map = new mapboxgl.Map({
       container: 'map',
       style: 'mapbox://styles/mapbox/streets-v11',
-      center: [-87.6244, 41.8756],
-      zoom: 13
+      center: [2.3399, 48.8555],
+      zoom: 5 // starting zoom
     });
   }
 
@@ -36,11 +36,128 @@ export class MapPage implements OnInit {
     this.map.on("load", () => {
       this.map.resize();
     });
-    this.map.addControl(new mapboxgl.NavigationControl());
+  this.map.addControl(new mapboxgl.NavigationControl(),'top  -left'); 
+    
+  }
+
+  mapDistance() {
+    var distanceContainer = document.getElementById('distance');
+
+    // GeoJSON object to hold our measurement features
+    var geojson = {
+      'type': 'FeatureCollection',
+      'features': []
+    };
+
+    // Used to draw a line between points
+    var linestring = {
+      'type': 'Feature',
+      'geometry': {
+        'type': 'LineString',
+        'coordinates': []
+      }
+    };
+
+    this.map.on('load', function () {
+      this.map.addSource('geojson', {
+        'type': 'geojson',
+        'data': geojson
+      });
+
+      // Add styles to the map
+      this.map.addLayer({
+        id: 'measure-points',
+        type: 'circle',
+        source: 'geojson',
+        paint: {
+          'circle-radius': 5,
+          'circle-color': '#000'
+        },
+        filter: ['in', '$type', 'Point']
+      });
+      this.map.addLayer({
+        id: 'measure-lines',
+        type: 'line',
+        source: 'geojson',
+        layout: {
+          'line-cap': 'round',
+          'line-join': 'round'
+        },
+        paint: {
+          'line-color': '#000',
+          'line-width': 2.5
+        },
+        filter: ['in', '$type', 'LineString']
+      });
+
+      this.map.on('click', function (e) {
+        var features = this.map.queryRenderedFeatures(e.point, {
+          layers: ['measure-points']
+        });
+
+        // Remove the linestring from the group
+        // So we can redraw it based on the points collection
+        if (geojson.features.length > 1) geojson.features.pop();
+
+        // Clear the Distance container to populate it with a new value
+        distanceContainer.innerHTML = '';
+
+        // If a feature was clicked, remove it from the map
+        if (features.length) {
+          var id = features[0].properties.id;
+          geojson.features = geojson.features.filter(function (point) {
+            return point.properties.id !== id;
+          });
+        } else {
+          var point = {
+            'type': 'Feature',
+            'geometry': {
+              'type': 'Point',
+              'coordinates': [e.lngLat.lng, e.lngLat.lat]
+            },
+            'properties': {
+              'id': String(new Date().getTime())
+            }
+          };
+
+          geojson.features.push(point);
+        }
+
+        if (geojson.features.length > 1) {
+          linestring.geometry.coordinates = geojson.features.map(
+            function (point) {
+              return point.geometry.coordinates;
+            }
+          );
+
+          geojson.features.push(linestring);
+
+          // Populate the distanceContainer with total distance
+          var value = document.createElement('pre');
+          value.textContent =
+            'Total distance: ' +
+            this.turf.length(linestring).toLocaleString() +
+            'km';
+          distanceContainer.appendChild(value);
+        }
+
+        this.map.getSource('geojson').setData(geojson);
+      });
+    });
+
+    this.map.on('mousemove', function (e) {
+      var features = this.map.queryRenderedFeatures(e.point, {
+        layers: ['measure-points']
+      });
+      // UI indicator for clicking/hovering a point on the map
+      this.map.getCanvas().style.cursor = features.length
+        ? 'pointer'
+        : 'crosshair';
+    });
   }
 
   mapLocation(){
-    this.map.addControl(
+   this.map.addControl(
       new mapboxgl.GeolocateControl({
       positionOptions: {
       enableHighAccuracy: true
@@ -48,33 +165,16 @@ export class MapPage implements OnInit {
       trackUserLocation: true
       })
       );
+     
   }
 
-  map3D(){
-    this.map.on('load', function() {
-      this.map.addLayer({
-          'id': '3d-buildings',
-          'source': 'composite',
-          'source-layer': 'building',
-          'filter': ['==', 'extrude', 'true'],
-          'type': 'fill-extrusion',
-          'minzoom': 15,
-          'paint': {
-              'fill-extrusion-color': '#aaa',
-              'fill-extrusion-height': {
-                  'type': 'identity',
-                  'property': 'height'
-              },
-              'fill-extrusion-base': {
-                  'type': 'identity',
-                  'property': 'min_height'
-              },
-              'fill-extrusion-opacity': .6
-          }
-      });
-  }); 
-
+  mapDirection(){
+    this.map.addControl(
+      new MapboxDirections({
+      accessToken: mapboxgl.accessToken
+      }),
+      'top-left'
+      );
   }
-
 
 }
